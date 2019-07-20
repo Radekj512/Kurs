52a4121ea73fb4d5bfbe5e7aba191a83dd9c0ce0
Radek <roszko.radek@gmail.com>
2019-07-20 16:54:55 +0200

    Skonczone zadanie JS

--- src/main/java/pl/sda/kurs/HTML_CSS_JS/resources/app.js
+++ src/main/java/pl/sda/kurs/HTML_CSS_JS/resources/app.js
@@ -2,31 +2,36 @@ $(document).ready(function () {
     const key = "gMABSLcnSGoDRZvgGnAe";
     getNotes();
     let flag = 0;
+    var notesMap = new Map();
+    let gloalId;
 
 
     $('form').submit(function (event) {
 
         let title = $('#validationCustom01').val();
         let content = $('#validationCustom02').val();
-        createNote(title, content);
+        if (gloalId === null) {
+            createNote(title, content);
+        }else{
+            updateNote(gloalId);
+        }
         event.preventDefault();
     });
 
-        $(document).on('click',".button_delete", function(){
-            let id = $(this).attr("note_id");
-            deleteNote(id);
+    $(document).on('click', ".button_delete", function () {
+        let id = $(this).attr("note_id");
+        deleteNote(id);
 
-        });
-        $(document).on('click', '.button_modify', function(){
-            let id = $(this).attr("note_id");
-            modifyNote(id);
-        });
+    });
+    $(document).on('click', '.button_modify', function () {
+        let id = $(this).attr("note_id");
+        modifyNote(id);
+    });
 
 
-        $(document).on('click', '#cancel_button', function(){
-            location.reload();
-        })
-
+    $(document).on('click', '#cancel_button', function () {
+        location.reload();
+    })
 
 
     const createNote = (title, content) => {
@@ -58,11 +63,13 @@ $(document).ready(function () {
             success: function (data) {
                 for (let i = 0; i < data.length; i++) {
                     addNote(data[i].title, data[i].content, data[i].id);
+                    notesMap.set(data[i].id, data[i]);
                 }
 
             }
         });
     }
+
     function deleteNote(noteId) {
         $.ajax({
             url: "http://178.62.193.183:8080/notes/" + noteId,
@@ -71,7 +78,7 @@ $(document).ready(function () {
                 "creator-name": key
             },
             type: "DELETE",
-            success:function(){
+            success: function () {
                 alert("Usunięto notatkę");
                 location.reload();
             }
@@ -82,35 +89,49 @@ $(document).ready(function () {
         $('#lab').html("Zmień notatkę");
         $('#submit_button').html("Zmień");
         $('#cancel_button').show();
-        $('#validationCustom01').val();
-        console.log($('#note_title').val());
-
-
+        let note = notesMap.get(noteId);
+        $('#validationCustom01').val(note.title);
+        $('#validationCustom02').val(note.content);
+        gloalId = noteId;
     }
-    function getById(noteId) {
-        $("#"+noteId).on("event", function () {
-            let title = $(this).attr("note_title");
-            console.log(title);
+
+
+    function updateNote(noteId) {
+        let title = $('#validationCustom01').val();
+        let content = $('#validationCustom02').val();
+        const data = {
+            content: content,
+            title: title
+        };
+        $.ajax({
+            url: "http://178.62.193.183:8080/notes/" + noteId,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "*/*",
+                "creator-name": key
+            },
+            data: JSON.stringify(data),
+            type: "PUT",
+            success:function(){
+                location.reload();
+            }
         });
     }
 
-    function updateNote(noteId){}
-
     function addNote(title, content, id) {
         let divClass = "row note2";
-        if(flag === 0){
+        if (flag === 0) {
             divClass = "row note";
             flag = 1;
-        }else{
+        } else {
             divClass = "row note2";
             flag = 0;
         }
-        $('#notes-list').append("<div class=\""+divClass+"\" id='"+id+"'>\n" +
-            "<div class=\"col-3\" note_title = \""+id+"\">" + title + "</div>" +
-            "<div class=\"col-7\" note_context = \""+id+"\">" + content + "</div>" +
-            "<div class=\"col-1\"><img src=\"modify.png\" class = \"button_modify\" note_id = \""+id+"\" ></div>" +
-            "<div class=\"col-1\"><img src=\"delete.png\" class = \"button_delete\" note_id = \""+id+"\" ></div>" +
+        $('#notes-list').append("<div class=\"" + divClass + "\" id='" + id + "'>\n" +
+            "<div class=\"col-3\" note_title = \"" + id + "\">" + title + "</div>" +
+            "<div class=\"col-5\" note_context = \"" + id + "\">" + content + "</div>" +
+            "<div class=\"col-2\"><img src=\"modify.png\" class = \"button_modify\" note_id = \"" + id + "\" ></div>" +
+            "<div class=\"col-2\"><img src=\"delete.png\" class = \"button_delete\" note_id = \"" + id + "\" ></div>" +
             "</div>");
     }
 });
-
